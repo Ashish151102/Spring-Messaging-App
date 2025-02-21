403fb66f8df38519c76f2fd198b3b6820ef29a3f
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-21 20:28:25 +0530

    [Ashish] Add .Add greeting with path variable.

--- src/main/java/com/example/Spring/Messaging/App/controller/HelloController.java
+++ src/main/java/com/example/Spring/Messaging/App/controller/HelloController.java
@@ -1,9 +1,6 @@
 package com.example.Spring.Messaging.App.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -16,6 +13,11 @@ public class HelloController {
 
     @GetMapping("/query")
     public String sayHelloWithName(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPathVariable(@PathVariable String name){
         return "Hello " + name + " from BridgeLabz";
     }
 }
