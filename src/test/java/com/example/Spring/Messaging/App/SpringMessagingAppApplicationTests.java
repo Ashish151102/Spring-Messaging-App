7eb32f8f275c24a0bbe751e73e248278e825f411
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-21 16:58:49 +0530

    [Ashish] Add . project created

---
+++ src/test/java/com/example/Spring/Messaging/App/SpringMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Messaging.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
