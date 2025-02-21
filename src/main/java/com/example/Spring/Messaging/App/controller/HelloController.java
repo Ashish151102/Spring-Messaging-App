ed5694bf15664beb82e5f033e66ed2049a89ac72
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-21 20:19:12 +0530

    [Ashish] Add . Add greeting with query parameter.

--- src/main/java/com/example/Spring/Messaging/App/controller/HelloController.java
+++ src/main/java/com/example/Spring/Messaging/App/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.Spring.Messaging.App.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz";
     }
 }
